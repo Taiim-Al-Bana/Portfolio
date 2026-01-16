145c3dada965bb25f00ccd41e952d03b6a9de837
Taiim-Al-Bana <1216413@student.roc-nijmegen.nl>
2026-01-16 12:27:22 +0100

    JS buttons en more works JS gedaan

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-/*dit is de eerste img van de homepage*/
-let isFirstImage = true;
+    /*dit is de eerste img van de homepage*/
+    let isFirstImage = true;
 
-function toggleImage(param) {
+    function toggleImage(param) {
 
     const imgProjects = document.getElementById("my-projects");
     const imgContact = document.getElementById("contact-me");
@@ -17,7 +17,7 @@ function toggleImage(param) {
             isFirstImage = true;
         }
     } else {
-       
+
     }
 
     if (param == 'contact-me') {
@@ -30,9 +30,9 @@ function toggleImage(param) {
             isFirstImage = true;
         }
     } else {
-       
-    }    
-  
+
+    }
+
     if (param == 'my-languages') {
 
         if (isFirstImage) {
@@ -43,7 +43,75 @@ function toggleImage(param) {
             isFirstImage = true;
         }
     } else {
-       
+
     }
-       
-}
+    }
+
+    /*dit is voor de works pagina*/
+    const ButtonPinkGoose = document.getElementById('Button-PinkGoose');
+    var open = false;
+
+    ButtonPinkGoose.addEventListener('click', () => {
+        const opentxt = document.getElementById('More-pinkgoose')
+        console.log("test");
+        if (!open){
+            opentxt.style.display = 'block'
+        } else if (open) {
+            opentxt.style.display = 'none'
+            open = false;
+        }
+    })
+
+    const ButtonMasterMind = document.getElementById('Button-MasterMind');
+    var open = false;
+
+    ButtonMasterMind.addEventListener('click', () => {
+        const opentxt = document.getElementById('More-mastermind')
+        console.log("test");
+        if (!open){
+            opentxt.style.display = 'block'
+        } else if (open) {
+            opentxt.style.display = 'none'
+            open = false;
+        }
+    })
+
+/* dit is voor de more works*/
+  
+ const button = document.querySelector('.more-works');
+    const extra = document.getElementById('extra-works');
+
+    button.addEventListener('click', () => {
+        extra.classList.toggle('hidden');
+    });
+
+       const ButtonConstruction = document.getElementById('Button-construction');
+    var open = false;
+
+    ButtonConstruction.addEventListener('click', () => {
+        const opentxt = document.getElementById('More-construction')
+        console.log("test");
+        if (!open){
+            opentxt.style.display = 'block'
+        } else if (open) {
+            opentxt.style.display = 'none'
+            open = false;
+        }
+    })
+
+     const ButtonConstruction2 = document.getElementById('Button-construction2');
+    var open = false;
+
+    ButtonConstruction2.addEventListener('click', () => {
+        const opentxt = document.getElementById('More-construction2')
+        console.log("test");
+        if (!open){
+            opentxt.style.display = 'block'
+        } else if (open) {
+            opentxt.style.display = 'none'
+            open = false;
+        }
+    })
+
+ 
+
